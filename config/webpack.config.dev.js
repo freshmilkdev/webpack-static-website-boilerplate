3e568d4472b078dfc04f138c94e632dc89259d8e
Max <max.ruchkin@gmail.com>
2018-06-23 17:31:07 +0300

    base prod and dev builds

---
+++ config/webpack.config.dev.js
@@ -0,0 +1,40 @@
+const webpack = require('webpack');
+const path = require('path');
+module.exports = {
+    //context: path.resolve(__dirname, "../"), // `__dirname` is root of project and `src` is source
+    entry: path.resolve(__dirname, "../src"),
+    output: {
+        path: path.resolve(__dirname, '../dist'), // `dist` is the destination
+        publicPath: "/assets/",
+        filename: 'bundle.js',
+    },
+    watch: true,
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                use: ['style-loader', 'css-loader']
+            },
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['env']
+                    }
+                }
+            }
+        ]
+    },
+    devServer: {
+        open: true, // to open the local server in browser
+        contentBase: path.resolve(__dirname, "../src/"),
+        hot: true
+    },
+    plugins: [
+        // Enable the plugin to let webpack communicate changes
+        // to WDS. --hot sets this automatically!
+        new webpack.HotModuleReplacementPlugin(),
+    ],
+};
---
+++ src/index.js
@@ -0,0 +1,2 @@
+const fancyES6Function = () => console.log('Transpile me to es5!');
+fancyES6Function();
