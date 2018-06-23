2658487c603287de852a0c045593dbf85e1d6866
Max <max.ruchkin@gmail.com>
2018-06-23 17:48:06 +0300

    prod build with minimized css

--- config/webpack.config.prod.js
+++ config/webpack.config.prod.js
@@ -1,9 +1,40 @@
 const path = require('path');
-
+const UglifyJsPlugin = require("uglifyjs-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const OptimizeCSSAssetsPlugin = require("optimize-css-assets-webpack-plugin");
 module.exports = {
     entry: path.resolve(__dirname, "../src"),
     output: {
         path: path.resolve(__dirname, '../dist'),
         filename: 'bundle.min.js'
+    },
+    optimization: {
+        minimizer: [
+            new UglifyJsPlugin({
+                cache: true,
+                parallel: true,
+                sourceMap: false // set to true if you want JS source maps
+            }),
+            new OptimizeCSSAssetsPlugin({})
+        ]
+    },
+    plugins: [
+        new MiniCssExtractPlugin({
+            // Options similar to the same options in webpackOptions.output
+            // both options are optional
+            filename: "[name].min.css",
+            chunkFilename: "[id].min.css"
+        })
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                use: [
+                    MiniCssExtractPlugin.loader,
+                    "css-loader"
+                ]
+            }
+        ]
     }
 };
--- src/index.js
+++ src/index.js
@@ -1,2 +1,3 @@
+import css from './css/style.css';
 const fancyES6Function = () => console.log('Transpile me to es5!');
 fancyES6Function();
